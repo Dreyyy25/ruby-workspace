2b2ff0a0f347a1d10cdf64c69a4f2e0e127f1eff
CorgiusMinimus <wendellravago@gmail.com>
2026-09-21 14:38:16 +0800

    feat: add ch2a5

---
+++ 02 - Fundamentals/05 - Range/range.rb
@@ -0,0 +1,13 @@
+arr = [2, 25, 33, 75, 100]
+
+# .min
+puts ".min: #{arr.min}"
+
+# .max 
+puts ".max: #{arr.max}"
+
+# .last
+puts ".last: #{arr.last}"
+
+# .include
+puts ".include: #{arr.include?(34)}"
