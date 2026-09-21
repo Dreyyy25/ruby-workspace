7167f90ac812aeb7164173c2f7524e16672d72a6
CorgiusMinimus <wendellravago@gmail.com>
2026-09-21 17:18:26 +0800

    feat: add ch2a11

---
+++ 02 - Fundamentals/12 - Objects/objects.rb
@@ -0,0 +1,16 @@
+t1 = {course: "Web Fundamentals Track", weeks: "2"}
+t2 = {course: "PHP Track", weeks: "4"}
+t3 = {course: "JS Track", weeks: "4"}
+t4 = {course: "Elective Track", weeks: "3"}
+
+tracks = [t1, t2, t3, t4]
+
+def iterate(arr)
+    puts "You have #{arr.length} courses in the 'tracks' array."
+    arr.each{ 
+        |obj|
+        puts "The course is #{obj[:course]} in #{obj[:weeks]} weeks"
+    }
+end
+
+iterate(tracks)
