f406a61c44d26759289bf7b6917fde2d8243a5c5
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-21 15:27:57 +0800

    refactor: redid my unless conditions

--- 02 - Fundamentals/01 - Guess the Number/guess.rb
+++ 02 - Fundamentals/01 - Guess the Number/guess.rb
@@ -1,14 +1,13 @@
 def guess_number (guess)
     number = 25
-    unless guess > number
-        puts "Guess was too low!"
-    end 
 
-    unless guess < number
-        puts "Guess was too high!"
-    end
-
-    unless guess != number
+    unless guess == number
+        if guess > number
+            puts "Guess was too high!"
+        else guess < number
+            puts "Guess was too low!"
+        end
+    else
         puts "You got it right!"
     end
 end
