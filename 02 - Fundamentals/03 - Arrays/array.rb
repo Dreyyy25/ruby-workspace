35d7ea0c53995edf5dc71c632b69bae177921d95
CorgiusMinimus <wendellravago@gmail.com>
2026-09-21 14:23:09 +0800

    feat: add ch2a3

---
+++ 02 - Fundamentals/03 - Arrays/array.rb
@@ -0,0 +1,31 @@
+arr = ["T-Rex", "Triceratops", "Brachiosaurus"]
+
+# .at
+puts ".at: #{arr.at(1)}"
+
+# .fetch
+puts ".fetch: #{arr.fetch(1)}"
+
+# .reverse
+puts ".reverse: #{arr.reverse}"
+
+# .length
+puts ".length: #{arr.length}"
+
+# .sort 
+puts ".sort: #{arr.sort}"
+
+# .slice
+puts ".slice: #{arr.slice(2)}"
+
+# .shuffle
+puts ".shuffle #{arr.shuffle}"
+
+# .join
+puts ".join #{arr.join(", ")}"
+
+# .insert
+puts ".insert #{arr.insert(2, "Pteranodon")}"
+
+# .values_at()
+puts ".values_at #{arr.values_at(1,2)}"
