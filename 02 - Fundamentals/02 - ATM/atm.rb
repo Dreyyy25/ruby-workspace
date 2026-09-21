ed6d8e57397e253688b02d5714a4add801751aec
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-21 15:44:54 +0800

    feat: added my code for this activity

---
+++ 02 - Fundamentals/02 - ATM/atm.rb
@@ -0,0 +1,17 @@
+def withdraw (digits)
+    number = 1000
+
+    unless digits >= 200 && digits <= number && digits <= 10000
+        if digits >= 10000
+            puts "Error. Maximum is 10000."
+        elsif digits <= 200
+            puts "Error. Minimum is 200."
+        elsif digits > number
+            puts "Error. Insuifficient balance."
+        end
+    else 
+        puts "Success. Your balance is now #{number - digits}."
+    end
+end
+
+withdraw(250)
