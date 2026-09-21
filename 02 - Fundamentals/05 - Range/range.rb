210baac6c4924adae2b1dce3f5539878d35162cb
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-21 16:55:12 +0800

    feat: added my code for this activity

---
+++ 02 - Fundamentals/05 - Range/range.rb
@@ -0,0 +1,16 @@
+range = (1..10)
+
+# .min
+# puts range.min
+
+# .max
+# puts range.max
+
+# .last
+# shuffled = range.to_a.shuffle
+# puts shuffled.to_s
+# puts "Last number is #{shuffled.last}"
+
+# .included?(value)
+# puts range.include?(10)
+# puts range.include?(15)
