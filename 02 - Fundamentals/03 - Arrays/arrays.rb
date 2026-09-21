80d6a475697841404b1d106c8b1853613827a38d
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-21 16:16:11 +0800

    feat: added my code for this activity

---
+++ 02 - Fundamentals/03 - Arrays/arrays.rb
@@ -0,0 +1,35 @@
+engineers = ["Wendell", "4th", "Andrey", "Giann", "Sean", "Ariel", "Michael", "James"]
+company = ["Altitude", "IQ"]
+
+# .at or .fetch
+# puts engineers.at(0)
+# puts engineers.fetch(2)
+
+# .delete
+# retired = engineers.delete("James")
+# puts retired
+# puts engineers
+
+# .reverse
+# puts engineers.reverse
+
+# .length
+# puts engineers.length
+
+# .sort
+# puts engineers.sort
+
+# .slice
+# puts engineers.slice(-1)
+
+# .shuffle
+# puts engineers.shuffle
+
+# .join
+# puts company.join
+
+# .insert
+# puts engineers.insert(0, "Jay")
+
+# values_at()
+# puts company.values_at(0, 1).join(' and ')
