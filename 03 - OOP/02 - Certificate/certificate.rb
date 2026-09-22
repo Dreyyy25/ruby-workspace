9ac5145f81ab3208386cc7d353eca4b4aff84000
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-22 15:47:32 +0800

    feat: added my code for this activity

---
+++ 03 - OOP/02 - Certificate/certificate.rb
@@ -0,0 +1,18 @@
+class Certificate
+
+    attr_accessor :title
+
+    def initialize(title, awardee)
+        @title = title
+        @awardee = awardee
+    end
+
+    def announce()
+        puts "#{@title}: #{@awardee}"
+    end
+
+end
+
+cert = Certificate.new("Proficient in Backend", "Philip Campani")
+puts cert.title
+cert.announce
