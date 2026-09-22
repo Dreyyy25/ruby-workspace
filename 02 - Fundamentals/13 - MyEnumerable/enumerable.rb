ed19124ee478037b24d7da91007444a434358919
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-22 13:58:40 +0800

    feat: added my code for this activity

---
+++ 02 - Fundamentals/13 - MyEnumerable/enumerable.rb
@@ -0,0 +1,16 @@
+module MyEnumerable
+  def my_each
+    # your code here!
+    for i in 0...self.length
+        yield self[i] # Added a yield to jump out of the loop
+    end
+    self
+  end
+end
+
+class Array
+   include MyEnumerable
+end
+
+[1,2,3,4].my_each { |i| puts i } # => 1 2 3 4
+[1,2,3,4].my_each { |i| puts i * 10 } # => 10 20 30 40
