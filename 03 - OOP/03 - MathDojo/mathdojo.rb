9746351b7e1071f90b76df02b067826ce07f9338
Miko-Mon <montecilloseanmikhail@gmail.com>
2026-09-22 16:13:00 +0800

    feat: added my code for this activity

---
+++ 03 - OOP/03 - MathDojo/mathdojo.rb
@@ -0,0 +1,21 @@
+class MathDojo
+    @@result = 0
+
+    def add(*params)
+        @@result += params.flatten.sum # Added flatten to merge arrays
+        self
+    end
+
+    def subtract(*params)
+        @@result -= params.flatten.sum # Added flatteb to merge arrays
+        self
+    end
+
+    def result
+        puts @@result
+    end
+
+end 
+
+challenge1 = MathDojo.new.add(2).add(2, 5).subtract(3, 2).result
+challenge2 = MathDojo.new.add(1).add([3, 5, 7, 8], [2, 4.3, 1.25]).subtract([2,3], [1.1, 2.3]).result 
